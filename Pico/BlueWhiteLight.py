a253ed909130a48ba54c38db501a755082ba79d8
charles_jennings <charlesjennings@zoho.com>
2021-10-21 23:11:11 -0500

    Added humidity alert

--- Pico/BlueWhiteLight.py
+++ Pico/BlueWhiteLight.py
@@ -2,9 +2,10 @@
 import machine
 import utime
 
-onboard_led_pin_id = 25
 blue_light = 15
 white_light = 16
+onboard_led_pin_id = 25
+
 light_on = 1
 light_off = 0
 
@@ -12,10 +13,15 @@ led_onboard = machine.Pin(onboard_led_pin_id, machine.Pin.OUT)
 blue_light_onboard = machine.Pin(blue_light, machine.Pin.OUT)
 white_light_onboard = machine.Pin(white_light, machine.Pin.OUT)
 
-# while True:
-#     led_onboard.toggle()
-#     utime.sleep(0.5)
+# Turn the onboard LED on so we know the device is running
+led_onboard.value()
 
-led_onboard.toggle()
-blue_light_onboard.toggle()
-white_light_onboard.toggle()
+while True:
+    blue_light_onboard.value(light_on)
+    utime.sleep(0.5)
+    blue_light_onboard.value(light_off)
+    utime.sleep(0.5)
+    white_light_onboard.value(light_on)
+    utime.sleep(0.5)
+    white_light_onboard.value(light_off)
+    utime.sleep(0.5)
---
+++ Pico/DHT11/ReadHumidityTemperature.py
@@ -0,0 +1,41 @@
+# Make it go blinky blink
+from machine import Pin, I2C
+import utime as time
+from DHT11 import DHT11, InvalidChecksum
+
+onboard_led_pin_id = 25 # Pin GPIO25
+max_humidity_for_warning = 55.0
+is_warning = 0
+onboard_light_pin = 15 # GPIO15
+humidity_warning_light = machine.Pin(onboard_light_pin, machine.Pin.OUT)
+
+led_onboard = machine.Pin(onboard_led_pin_id, machine.Pin.OUT)
+
+# Turn the onboard LED on so we know the device is running
+led_onboard.value(1)
+
+while True:
+    if is_warning == 1:
+        while True:
+            humidity_warning_light.value(1)
+            time.sleep(0.1)
+            humidity_warning_light.value(0)
+            time.sleep(0.1)
+            humidity_warning_light.value(1)
+            time.sleep(0.1)
+            humidity_warning_light.value(0)
+            time.sleep(0.1)
+    else:
+        humidity_warning_light.value(0)
+    time.sleep(5)
+    pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
+    sensor = DHT11(pin)
+    t  = (sensor.temperature)
+    h = (sensor.humidity)
+    print("Temperature: {}".format(sensor.temperature))
+    print("Humidity: {}".format(sensor.humidity))
+    
+    if h > max_humidity_for_warning:
+        is_warning = 1
+    else:
+        is_warning = 0
