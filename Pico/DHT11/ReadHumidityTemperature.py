a44f1c3ca97c028fa72186d5120662a1d6273d42
charles_jennings <charlesjennings@zoho.com>
2021-10-22 17:26:44 -0500

    Bug fix for infinite loop

--- Pico/DHT11/ReadHumidityTemperature.py
+++ Pico/DHT11/ReadHumidityTemperature.py
@@ -8,34 +8,48 @@ max_humidity_for_warning = 55.0
 is_warning = 0
 onboard_light_pin = 15 # GPIO15
 humidity_warning_light = machine.Pin(onboard_light_pin, machine.Pin.OUT)
+pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
+sensor = DHT11(pin)
 
 led_onboard = machine.Pin(onboard_led_pin_id, machine.Pin.OUT)
 
 # Turn the onboard LED on so we know the device is running
 led_onboard.value(1)
 
+def ControlLight(light_state):
+	if light_state == 1:
+		while True:
+			humidity_warning_light.value(1)
+			time.sleep(0.1)
+			humidity_warning_light.value(0)
+			time.sleep(0.1)
+			humidity_warning_light.value(1)
+			time.sleep(0.1)
+			humidity_warning_light.value(0)
+			time.sleep(0.1)
+			humidity_warning_light.value(1)
+			time.sleep(0.1)
+			humidity_warning_light.value(0)
+			time.sleep(0.1)
+			humidity_warning_light.value(1)
+			time.sleep(0.1)
+			humidity_warning_light.value(0)
+			time.sleep(0.1)
+			break
+	else:
+		humidity_warning_light.value(0)
+
 while True:
-    if is_warning == 1:
-        while True:
-            humidity_warning_light.value(1)
-            time.sleep(0.1)
-            humidity_warning_light.value(0)
-            time.sleep(0.1)
-            humidity_warning_light.value(1)
-            time.sleep(0.1)
-            humidity_warning_light.value(0)
-            time.sleep(0.1)
-    else:
-        humidity_warning_light.value(0)
-    time.sleep(5)
-    pin = Pin(28, Pin.OUT, Pin.PULL_DOWN)
-    sensor = DHT11(pin)
-    t  = (sensor.temperature)
-    h = (sensor.humidity)
-    print("Temperature: {}".format(sensor.temperature))
-    print("Humidity: {}".format(sensor.humidity))
-    
-    if h > max_humidity_for_warning:
-        is_warning = 1
-    else:
-        is_warning = 0
+	if is_warning == 1:
+		ControlLight(1)
+	else:
+		ControlLight(0)
+
+	time.sleep(5)
+	print("Temperature: {}".format(sensor.temperature))
+	print("Humidity: {}".format(sensor.humidity))
+	
+	if sensor.humidity > max_humidity_for_warning:
+		is_warning = 1
+	else:
+		is_warning = 0
