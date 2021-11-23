3f63011ee82d4d5806fb6d833eb8b324467396ee
charles_jennings <charlesjennings@zoho.com>
2021-11-22 19:12:46 -0600

    Improved pi W sensor readings. Old DHT deprecated

--- RaspberryPi/ZeroW/DHT11.py
+++ RaspberryPi/ZeroW/DHT11.py
@@ -1,16 +1,24 @@
-# https://www.thegeekpub.com/236867/using-the-dht11-temperature-sensor-with-the-raspberry-pi/
+# https://medium.com/initial-state/how-to-build-a-raspberry-pi-temperature-monitor-8c2f70acaea9
 # After fresh RPi OS install run the following
-# sudo pip3 install Adafruit_DHT
-import Adafruit_DHT
+# sudo pip3 install adafruit-circuitpython-dht
+# sudo apt-get install libgpiod2
+# Next: https://www.w3schools.com/python/ref_requests_post.asp
+import adafruit_dht
 import time
+import board
 
-DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 14 # Board position #8, fourth down from top right
+DHT_PIN = board.D14 # Board position #8, fourth down from top right
+dhtSensor = adafruit_dht.DHT11(DHT_PIN)
 
 while True:
-    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}F Humidity={1:0.1f}%".format(round((temperature * 1.8) + 32), humidity))
-    else:
+    try:
+        temp_c = dhtSensor.temperature
+        humidity = dhtSensor.humidity
+        if humidity is not None and temp_c is not None:
+            print("Temp={0:0.1f}F Humidity={1:0.1f}%".format(round((temp_c * 1.8) + 32), humidity))
+        else:
+            print("Sensor failure. Check wiring.")
+    except:
         print("Sensor failure. Check wiring.")
-    time.sleep(5)
+    finally:
+        time.sleep(5)
